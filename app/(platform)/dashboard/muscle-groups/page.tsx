795acf1e02a150b8760056817a0a4e43d42a2ea1
Supuky <saito.hajime0112@gmail.com>
2024-03-19 15:30:54 +0900

    useParamsによるビルドのエラーを無効化

--- app/(platform)/dashboard/muscle-groups/page.tsx
+++ app/(platform)/dashboard/muscle-groups/page.tsx
@@ -6,7 +6,6 @@ import Link from 'next/link';
 import { Dumbbell } from 'lucide-react';
 import { MuscleGroup } from '@/types/workout';
 import useApi from '@/app/_hooks/useApi';
-import { Suspense } from 'react'
 
 interface ApiResponse {
   muscleGroups: MuscleGroup[],
@@ -33,22 +32,20 @@ const MuscleGroupsPage = () => {
   }, []);
 
   return (
-    <Suspense>
-      <div className="flex justify-center items-center gap-8 flex-wrap max-w-[600px] mt-10">
-        {
-          muscleGroups?.map(muscle => (
-            <Link 
-              key={muscle.id}
-              href={`/dashboard/muscle-groups/${muscle.id}/exercise?date=${date}`}
-              className="w-[120px] p-8 flex flex-col items-center gap-2 bg-white rounded-xl shadow-xl text-base-black/90 hover:opacity-80"
-            >
-                <Dumbbell size={30} className="rotate-[135deg] text-primary"/>
-                {muscle.name}
-            </Link>
-          ))
-        }
-      </div>  
-    </Suspense>
+    <div className="flex justify-center items-center gap-8 flex-wrap max-w-[600px] mt-10">
+      {
+        muscleGroups?.map(muscle => (
+          <Link 
+            key={muscle.id}
+            href={`/dashboard/muscle-groups/${muscle.id}/exercise?date=${date}`}
+            className="w-[120px] p-8 flex flex-col items-center gap-2 bg-white rounded-xl shadow-xl text-base-black/90 hover:opacity-80"
+          >
+              <Dumbbell size={30} className="rotate-[135deg] text-primary"/>
+              {muscle.name}
+          </Link>
+        ))
+      }
+    </div>  
   );
 };
 
