58af751393333fac4906269a6e9140796b7b9d77
Supuky <saito.hajime0112@gmail.com>
2024-03-20 09:21:43 +0900

    css変更

--- app/(platform)/dashboard/_components/DashboardSkeleton.tsx
+++ app/(platform)/dashboard/_components/DashboardSkeleton.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 function DashboardSkeleton() {
   return (
     <div className="animate-pulse">
-      <div className="max-w-[600px] h-80 bg-base-white rounded-xl shadow-xl md:w-[600px] md:m-auto"></div>
+      <div className="max-w-[600px] h-80 bg-base-white rounded-xl shadow-xl m-auto md:w-[600px]"></div>
       <div className="mt-8 h-14 bg-base-white rounded-xl shadow-xl max-w-[600px] m-auto md:w-[600px]"></div>
     </div>
   );
