91a5a139abbbde0fd23671574caa38d419c9abcd
Supuky <saito.hajime0112@gmail.com>
2024-03-20 13:48:32 +0900

    feature/updateAuth ログイン・新規登録画面の調整

--- app/(auth)/_components/Form.tsx
+++ app/(auth)/_components/Form.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import Image from 'next/image';
 import React from 'react';
+import { Mail, KeyRound } from 'lucide-react';
 
 interface Props {
   mode : "signup" | "login",
@@ -20,49 +22,68 @@ const Form: React.FC<Props> = ({
   onSubmit
 }) => {
   return (
-    <div className="flex justify-center pt-[240px]">
-      <form onSubmit={onSubmit} className="space-y-4 w-full max-w-[400px]">
-        <div>
-          <label
-            htmlFor="email"
-            className="block mb-2 text-sm font-medium text-gray-900"
-          >
-            メールアドレス
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary focus-visible:outline-primary block w-full p-2.5"
-            placeholder="name@company.com"
-            required
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
+    <div className="h-svh flex justify-center items-center">
+      <form onSubmit={onSubmit} className=" w-4/5 max-w-[600px] bg-base-white/80 p-8 rounded-xl shadow-xl text-base-black">
+        <div className='mb-8 flex gap-2 items-center justify-center'>
+          <Image 
+            src="/logo.svg"
+            alt="Logo"
+            height={34}
+            width={34}
           />
+          <h2 className="font-bold text-primary text-xl">FitTrack</h2>
         </div>
-        <div>
-          <label
+        <div className="mb-4">
+          <div className="relative">
+            {/* <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              メールアドレス
+            </label> */}
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+              <Mail size={20} className="text-gray-400"/>
+            </div>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-base-black/90 text-sm rounded-lg focus:ring-primary focus:border-primary focus-visible:outline-primary block w-full ps-10 p-2"
+              placeholder="メールアドレス"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            </div>
+        </div>
+        <div className="mb-4">
+          {/* <label
             htmlFor="password"
             className="block mb-2 text-sm font-medium text-gray-900"
           >
             パスワード
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="••••••••"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary focus-visible:outline-primary block w-full p-2.5"
-            required
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-          />
+          </label> */}
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+              <KeyRound size={20} className="text-gray-400"/>
+            </div>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="パスワード"
+              className="bg-gray-50 border border-gray-300 text-base-black/90 text-sm rounded-lg focus:ring-primary focus:border-primary focus-visible:outline-primary block w-full ps-10 p-2 "
+              required
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </div>
         </div>
 
         <div>
           <button
             type="submit"
-            className="w-full text-white bg-primary hover:bg-primary-pale focus:ring-4 focus:outline-none focus:ring-primary-pale focus-visible:outline-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            className="w-full text-white bg-primary hover:bg-primary-pale focus:ring-4 focus:outline-none focus:ring-primary-pale focus-visible:outline-primary font-medium rounded-lg text-sm px-5 py-2 text-center"
           >
             {
               mode === "signup" ? "新規登録" : "ログイン"
