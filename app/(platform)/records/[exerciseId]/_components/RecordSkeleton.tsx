8e82d1a13b9023121d637f34cc75b900fd2b7a2a
Supuky <saito.hajime0112@gmail.com>
2024-03-28 15:46:53 +0900

    スケルトン修正

--- app/(platform)/records/[exerciseId]/_components/RecordSkeleton.tsx
+++ app/(platform)/records/[exerciseId]/_components/RecordSkeleton.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 function RecordSkeleton() {
   return (
     <div className="animate-pulse">
-      <div className="my-4 h-14 bg-base-white rounded-xl shadow-xl max-w-[600px]"></div>
-      <div className="h-[50svh] bg-base-white rounded-xl shadow-xl max-w-[600px]"></div>
+      <div className="my-4 h-14 bg-base-white rounded-xl shadow-xl max-w-[600px] m-auto"></div>
+      <div className="h-[50svh] bg-base-white rounded-xl shadow-xl max-w-[600px] m-auto"></div>
       <div className="mt-4 m-auto w-10/12 h-[34px] bg-base-white rounded-xl shadow-xl max-w-[600px]"></div>
     </div>
   );
